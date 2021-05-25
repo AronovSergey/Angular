aef4e03aceeccbb5db5fc03bed43ee6590a72001
Sergey Aronov <AronovSergeyy@gmail.com>
2021-05-25 17:40:38 +0300

    Property-Binding-And-Event-Binding

---
+++ 02.Property-Binding-And-Event-Binding/buttons/buttons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-buttons',
+    templateUrl: './buttons.component.html',
+})
+export class ButtonsComponent {
+    isDisabled: boolean = false;
+    onButtonClick = () => {
+        this.isDisabled = !this.isDisabled;
+    };
+}
